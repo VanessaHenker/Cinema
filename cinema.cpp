3428d7d7226c4f268af9438a2b8b9a2620a1e884
Vanessa <vs6615302@gmail.com>
2023-06-27 14:59:25 -0300

    Adicionando a função polt_selecionada na sessao_2

--- cinema.cpp
+++ cinema.cpp
@@ -669,15 +669,7 @@ void sessao_2(){
 
   system("cls");
   if (reserva_polt == 'n' || reserva_polt == 'N'){
-    cout << "\nAssentos selecionados: ";
-    for (i = 0; i < 12; i++){
-      for (j = 0; j < 6; j++){
-        if (cadeiras2[i][j] == "[//]"){
-          cout << i << j << ", ";
-          guarda_polt++;
-        }
-      }
-    }
+    polt_selecionada();
     if (guarda_polt > 0){
       compra_ing(guarda_polt);
     }
