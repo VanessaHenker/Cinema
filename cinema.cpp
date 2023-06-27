b302eeb14e09848712ecd0d7a1f15ef8378fe6ec
Vanessa <vs6615302@gmail.com>
2023-06-26 21:28:19 -0300

    Alteração na função sessao_3

--- cinema.cpp
+++ cinema.cpp
@@ -819,6 +819,13 @@ void sessao_3(){
     else{
       cout << "\nNenhuma poltrona foi selecionada!" << endl;
     }
+    for (i = 0; i < 10; i++){
+      for (j = 0; j < 5; j++){
+        if (cadeiras1[i][j] == "[//]"){
+          cadeiras1[i][j] = "[--]";
+        }
+      }
+    }
   }
 }
 void sessao_4(){
